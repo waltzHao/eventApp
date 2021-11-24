12ad5887949d50d0448a4d4e8d387c35e9f92695
waltzHao <waltzhao828@gmail.com>
2021-11-24 12:46:03 +0800

    1. Add user module 2. Add event module 3. Add namespace for event 4. use mapActions for event show

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import EventService from "@/services/EventService.js";
+
+import * as user from '@/store/modules/user.js';
+import * as event from '@/store/modules/event.js';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  modules: {
+    user,
+    event
+  },
   state: {
-    user: { id: "abc123", name: "Anivia" },
+   
     categories: [
       "sustainability",
       "nature",
@@ -15,60 +22,5 @@ export default new Vuex.Store({
       "food",
       "community",
     ],
-    events: [],
-    eventsTotal: 0,
-    event: {},
-  },
-  mutations: {
-    ADD_EVENT(state, event) {
-      state.events.push(event);
-    },
-    SET_EVENTS(state, events) {
-      state.events = events;
-    },
-    SET_EVENTS_TOTAL(state, total) {
-      state.eventsTotal = total;
-    },
-    SET_EVENT(state, event) {
-      state.event = event;
-    },
-  },
-  actions: {
-    createEvent({ commit }, event) {
-      return EventService.postEvent(event).then(() => {
-        commit("ADD_EVENT", event);
-      });
-    },
-    fetchEvents({ commit }, { perPage, page }) {
-      EventService.getEvents(perPage, page)
-        .then((response) => {
-          console.log("Total events are " + response.headers["x-total-count"]);
-          commit("SET_EVENTS_TOTAL", response.headers["x-total-count"]);
-          commit("SET_EVENTS", response.data);
-        })
-        .catch((error) => {
-          console.log("There was an error" + error);
-        });
-    },
-    fetchEvent({ commit, getters }, id) {
-      var event = getters.getEventById(id);
-
-      if (event) {
-        commit("SET_EVENT", event);
-      } else {
-        EventService.getEvent(id)
-          .then((response) => {
-            commit("SET_EVENT", response.data);
-          })
-          .catch((error) => {
-            console.log("There was an error:", error.response);
-          });
-      }
-    },
-  },
-  getters: {
-    getEventById: (state) => (id) => {
-      return state.events.find((event) => event.id === id);
-    },
-  },
+  }
 });
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -3,7 +3,7 @@
     <div class="event-header">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h1 class="title">{{ event.title }}</h1>
-      <h5>Organized by {{ event.organizer ? event.organizer.name : '' }}</h5>
+      <h5>Organized by {{ event.organizer ? event.organizer.name : "" }}</h5>
       <h5>Category: {{ event.category }}</h5>
     </div>
     <BaseIcon name="map"><h2>Location</h2></BaseIcon>
@@ -12,7 +12,9 @@
     <p>{{ event.description }}</p>
     <h2>
       Attendees
-      <span class="badge -fill-gradient">{{ event.attendees ? event.attendees.length : 0 }}</span>
+      <span class="badge -fill-gradient">{{
+        event.attendees ? event.attendees.length : 0
+      }}</span>
     </h2>
     <ul class="list-group">
       <li
@@ -26,33 +28,34 @@
   </div>
 </template>
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions } from "vuex";
 export default {
   props: ["id"],
-  
+
   created() {
-    this.$store.dispatch('fetchEvent', this.id)
+    this.$store.dispatch("event/fetchEvent", this.id);
   },
-  computed: mapState(['event'])
+  computed: mapState({ event: (state) => state.event.event }),
+  methods: mapActions(['event/fetchEvent'])
 };
 </script>
  <style scoped>
-    .location {
-      margin-bottom: 0;
-    }
-    .location > .icon {
-      margin-left: 10px;
-    }
-    .event-header > .title {
-      margin: 0;
-    }
-    .list-group {
-      margin: 0;
-      padding: 0;
-      list-style: none;
-    }
-    .list-group > .list-item {
-      padding: 1em 0;
-      border-bottom: solid 1px #e5e5e5;
-    }
-    </style>
+.location {
+  margin-bottom: 0;
+}
+.location > .icon {
+  margin-left: 10px;
+}
+.event-header > .title {
+  margin: 0;
+}
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+</style>
